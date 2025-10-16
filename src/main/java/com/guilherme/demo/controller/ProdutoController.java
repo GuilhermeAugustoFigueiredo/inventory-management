f5eb78b09f60bb29042013693720c84222af539f
GuilhermeAugustoFigueiredo <guilherme.figueiredo@sptech.school>
2025-10-16 17:52:12 -0300

    feat: adicionando ProdutoController e UsuarioController

--- src/main/java/com/guilherme/demo/controller/ProdutoController.java
+++ src/main/java/com/guilherme/demo/controller/ProdutoController.java
@@ -1,4 +1,85 @@
 package com.guilherme.demo.controller;
 
+import com.guilherme.demo.dto.ProdutoDto.ProdutoAtualizacaoDto;
+import com.guilherme.demo.dto.ProdutoDto.ProdutoCadastroDto;
+import com.guilherme.demo.dto.ProdutoDto.ProdutoListagemDto;
+import com.guilherme.demo.dto.ProdutoDto.ProdutoMapper;
+import com.guilherme.demo.entity.Produto;
+import com.guilherme.demo.service.ProdutoService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+@RequiredArgsConstructor
 public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoListagemDto>> listar(){
+        List<Produto> produtos = produtoService.listar();
+
+        if (produtos.isEmpty()){
+            return ResponseEntity.status(204).build();
+        }
+
+        List<ProdutoListagemDto> produtosResponse = ProdutoMapper.toListagemDtos(produtos);
+
+        return ResponseEntity.status(200).body(produtosResponse);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoListagemDto> buscarPorId(@PathVariable Long id){
+        Produto produtoFound = produtoService.buscarPorId(id);
+        var produtoFoundResponse = ProdutoMapper.toListagemDto(produtoFound);
+        return ResponseEntity.status(200).body(produtoFoundResponse);
+    }
+
+    @GetMapping("/nome")
+    public ResponseEntity<ProdutoListagemDto> buscarPorNome(@RequestParam String nome){
+        var produtoFound = produtoService.buscarPorNome(nome);
+        var produtoResponse = ProdutoMapper.toListagemDto(produtoFound);
+        return ResponseEntity.status(200).body(produtoResponse);
+    }
+
+    @GetMapping("/marca")
+    public ResponseEntity<List<ProdutoListagemDto>> buscarPorMarca(@RequestParam String marca){
+        var produtosFound = produtoService.buscarPorMarca(marca);
+        var produtosResponse = ProdutoMapper.toListagemDtos(produtosFound);
+        return ResponseEntity.status(200).body(produtosResponse);
+    }
+
+    @GetMapping("/categoria")
+    public ResponseEntity<List<ProdutoListagemDto>> buscarPorCategoria(@RequestParam String categoria){
+        var produtosFound = produtoService.buscarPorMarca(categoria);
+        var produtosResponse = ProdutoMapper.toListagemDtos(produtosFound);
+        return ResponseEntity.status(200).body(produtosResponse);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoListagemDto> cadastrar(@Valid @RequestBody ProdutoCadastroDto produtoRequest){
+        Produto produto = ProdutoMapper.toEntity(produtoRequest);
+        var produtoRegister = produtoService.cadastrar(produto);
+        ProdutoListagemDto produtoResponse = ProdutoMapper.toListagemDto(produtoRegister);
+        return ResponseEntity.status(201).body(produtoResponse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProdutoListagemDto> atualizar(@PathVariable Long id, @Valid @RequestBody ProdutoAtualizacaoDto produtoRequest) {
+        Produto produto = ProdutoMapper.toEntity(produtoRequest, id);
+        Produto produtoUpdated = produtoService.atualizar(produto);
+        ProdutoListagemDto produtoResponse = ProdutoMapper.toListagemDto(produtoUpdated);
+        return ResponseEntity.status(200).body(produtoResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remover(@PathVariable Long id){
+        produtoService.removerPorId(id);
+        return ResponseEntity.status(204).build();
+    }
 }
--- src/main/java/com/guilherme/demo/controller/UsuarioController.java
+++ src/main/java/com/guilherme/demo/controller/UsuarioController.java
@@ -1,4 +1,71 @@
 package com.guilherme.demo.controller;
 
+import com.guilherme.demo.dto.UsuarioDto.UsuarioAtualizacaoDto;
+import com.guilherme.demo.dto.UsuarioDto.UsuarioCadastroDto;
+import com.guilherme.demo.dto.UsuarioDto.UsuarioListagemDto;
+import com.guilherme.demo.dto.UsuarioDto.UsuarioMapper;
+import com.guilherme.demo.entity.Usuario;
+import com.guilherme.demo.service.UsuarioService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+@RequiredArgsConstructor
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioListagemDto>> listar(){
+        List<Usuario> usuarios = usuarioService.listar();
+
+        if (usuarios.isEmpty()){
+            return ResponseEntity.status(204).build();
+        }
+
+        List<UsuarioListagemDto> usuariosResponse = UsuarioMapper.toListagemDtos(usuarios);
+
+        return ResponseEntity.status(200).body(usuariosResponse);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioListagemDto> buscarPorId(@PathVariable Long id){
+        Usuario usuarioFound = usuarioService.buscarPorId(id);
+        var usuarioFoundResponse = UsuarioMapper.toListagemDto(usuarioFound);
+        return ResponseEntity.status(200).body(usuarioFoundResponse);
+    }
+
+    @GetMapping("/cargo")
+    public ResponseEntity<List<UsuarioListagemDto>> buscarPorCargo(@RequestParam String cargo){
+        List<Usuario> usuariosFound = usuarioService.buscarPorCargo(cargo);
+        var usuariosFoundResponse = UsuarioMapper.toListagemDtos(usuariosFound);
+        return ResponseEntity.status(200).body(usuariosFoundResponse);
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioListagemDto> cadastrar(@Valid @RequestBody UsuarioCadastroDto usuarioRequest){
+        Usuario usuario = UsuarioMapper.toEntity(usuarioRequest);
+        var usuarioRegister = usuarioService.cadastrar(usuario);
+        UsuarioListagemDto usuarioResponse = UsuarioMapper.toListagemDto(usuarioRegister);
+        return ResponseEntity.status(201).body(usuarioResponse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioListagemDto> atualizar(@PathVariable Long id, @Valid @RequestBody UsuarioAtualizacaoDto usuarioRequest) {
+        Usuario usuario = UsuarioMapper.toEntity(usuarioRequest, id);
+        Usuario usuarioUpdated = usuarioService.atualizar(usuario);
+        UsuarioListagemDto usuarioResponse = UsuarioMapper.toListagemDto(usuarioUpdated);
+        return ResponseEntity.status(200).body(usuarioResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remover(@PathVariable Long id){
+        usuarioService.removerPorId(id);
+        return ResponseEntity.status(204).build();
+    }
 }
